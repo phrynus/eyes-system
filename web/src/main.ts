86950e67f52e70320fa06d0f82fd9d2728c2c43d
phrtynus <fondantcn@gmail.com>
2024-05-30 18:55:43 +0800

    gx

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,14 +1,21 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import App from './App.vue'
-import router from './router'
+import ElementPlus from "element-plus";
+import zhCn from "element-plus/es/locale/lang/zh-cn";
+import "element-plus/dist/index.css";
 
-const app = createApp(App)
+import App from "./App.vue";
+import router from "./router";
 
-app.use(createPinia())
-app.use(router)
+const app = createApp(App);
 
-app.mount('#app')
+app.use(createPinia());
+app.use(ElementPlus, {
+  locale: zhCn
+});
+app.use(router);
+
+app.mount("#app");
